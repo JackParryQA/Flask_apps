a5f971e6fe07e2d8491263c2fc94c725d55d043e
Jack Parry <JParry@qa.com>
2021-05-11 14:13:19 +0100

    added another table to schema tutorial

--- schema_tutorial/app.py
+++ schema_tutorial/app.py
@@ -1,3 +1,4 @@
+from enum import unique
 from flask import Flask # Import Flask class
 from flask_sqlalchemy import SQLAlchemy # Import SQLAlchemy class
 
@@ -11,6 +12,23 @@ class Users(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     firstname=db.Column(db.String(100), nullable=False)
     surname=db.Column(db.String(100), nullable=False)
+
+# schema exercise -- these tables will be used in my project
+# class Customers(db.Model):
+#     customer_id=db.Column(db.Integer, primary_key=True)
+#     firstname=db.Column(db.String(100), nullable=False)
+#     surname=db.Column(db.String(100), nullable=False)
+#     email=db.Column(db.String(50), nullable=False, unique=True)
+#     phone_num=db.Column(db.String(20), nullable=False, unique=True)
+#     address=db.Column(db.String(200), nullable=False)
+#     postcode=db.Column(db.String(10), nullable=False)
+
+
+class Job(db.Model):
+    job_id=db.Column(db.Integer, primary_key=True)
+    customer_id=db.Column(db.Integer, nullable=False)
+    description=db.Column(db.String(200), nullable=False)
+    price=db.Column(db.Float(10,2), nullable=False, default=0.00)
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
--- schema_tutorial/create.py
+++ schema_tutorial/create.py
@@ -1,8 +1,12 @@
-from app import db, Users
+
+from app import db, Users, Job
 
 db.drop_all()
 db.create_all()
 
 testuser = Users(firstname='Grooty',surname='Toot') # Extra: this section populates the table with an example entry
+# testcustomer = Customers('Jack','Parry','jp@hotmail.com','07777 777777','123 Street','LL199AA')
+testjob = Job(customer_id=1,description='Do this job')
 db.session.add(testuser)
+db.session.add(testjob)
 db.session.commit()
