22014a26a0a3a00073abc6da75145987386a5d78
Jack Parry <JParry@qa.com>
2021-05-18 14:22:04 +0100

    changed ip for database

--- TODO_List/application/__init__.py
+++ TODO_List/application/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@35.246.31.25:3306/todo' 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@34.105.250.244:3306/todo' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
 
